6053617bb25afe5bb5d49216f0080f2071b9fc7c
mukuls1107 <mukulsingh3078@gmail.com>
2025-05-11 17:56:38 +0530

    Add: Contact links are in queue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import "./App.css"
+import "./App.css";
 import Title from "./components/Title";
+import Nav from "./components/Nav";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab, faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, fab, faTwitter, faGithub, faLinkedin)
+
 function App() {
   return (
     <>
-      <Title name="Mukul Singh" age="20" classname="test" /> 
+      <Title name="Mukul Singh" age="20" classname="test" />
     </>
   );
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import "../styles/title.css"
+import React from "react";
+import "../styles/title.css";
 // Import the image properly
-import profileImage from "../assets/portfolio2.png"
-
-
+import profileImage from "../assets/portfolio2.png";
+import Nav from "./Nav";
 
 function Title(props) {
   return (
     <div className="section-box">
       <div className="content-side">
-        <p className="clr-light">Hi, my name is</p>
-        <h1 className="head">{props.name}</h1>
-        <p className="blur extend">I build things for the web.</p>
-        <p className='blur'>I'm a {props.age} year old developer living in India, who got into coding trying to make a simple discord bot. Eventually, my interest grew and started exploring different parts of the CS world.  </p>
+        <div>
+          <p className="clr-light">Hi, my name is</p>
+          <h1 className="head">{props.name}</h1>
+          <p className="blur extend">I build things for the web.</p>
+          <p className="blur">
+            I'm a {props.age} year old developer living in India, who got into
+            coding trying to make a simple discord bot. Eventually, my interest
+            grew and started exploring different parts of the CS world.{" "}
+          </p>
+        </div>
+        <Nav />
       </div>
-      
+
       <div className="image-side">
-        <div className='imgBg'></div>
-        <img 
-          src={profileImage} 
-          alt="Mukul Singh" 
-          className="profile-image" 
-        />
+        <div className="imgBg"></div>
+        <img src={profileImage} alt="Mukul Singh" className="profile-image" />
       </div>
     </div>
   );
 }
 
-export default Title
-
+export default Title;
